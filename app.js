d88d159c6d741545f424018af45334105e41d293
BrunoSDias <brunosdias@outlook.com>
2020-05-23 15:17:50 -0300

    cross domain header allow

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD");
+  res.header("Access-Control-Request-Method", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
   next();
 })
